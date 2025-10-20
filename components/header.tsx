bab62b158d803b592d2cd0c04bf580316e333a34
daniblagojevic <140380411+daniblagojevic@users.noreply.github.com>
2025-10-20 16:19:47 +0200

    Remove empty line before Collapse Button
    Removed an empty line before the Collapse Button section.

--- components/header.tsx
+++ components/header.tsx
@@ -17,7 +17,6 @@ export default function Head() {
                                         </div>
                                     </Link>
 
-
                                     {/* Collapse Button */}
                                     <button type="button" className="hs-collapse-toggle md:hidden relative size-9 flex justify-center items-center font-medium rounded-lg border border-gray-200 disabled:opacity-50 disabled:pointer-events-none" id="hs-header-base-collapse" aria-expanded="false" aria-controls="hs-header-base" aria-label="Toggle navigation" data-hs-collapse="#hs-header-base" >
                                         <svg className="hs-collapse-open:hidden size-4" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"><line x1="3" x2="21" y1="6" y2="6" /><line x1="3" x2="21" y1="12" y2="12" /><line x1="3" x2="21" y1="18" y2="18" /></svg>
