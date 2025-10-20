75135e7ce6e51859f9c121cd662c14c379d2dd79
daniblagojevic <140380411+daniblagojevic@users.noreply.github.com>
2025-10-20 16:29:02 +0200

    Remove Financial Services link from footer
    Removed 'Financial Services' link from footer.

--- components/footer.tsx
+++ components/footer.tsx
@@ -65,7 +65,6 @@ export default function Footer() {
                             <h4 className="mt-7 text-xs font-semibold text-gray-900 uppercase">Industries</h4>
 
                             <div className="mt-3 grid space-y-3 text-sm">
-                                <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-hidden focus:text-gray-800" href="#">Financial Services</a></p>
                                 <p><a className="inline-flex gap-x-2 text-gray-600 hover:text-gray-800 focus:outline-hidden focus:text-gray-800" href="#">Education</a></p>
                             </div>
                         </div>
